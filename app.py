7489aa8784214830902ac679f165d7dace28c28a
ViniciusAmante <vini25henrique@gmail.com>
2025-11-13 18:05:39 -0300

    Commit inicial

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('curriculo.html')
+
+@app.route('/projetos')
+def projetos():
+    return render_template('projetos.html')
+
+@app.route('/habilidades')
+def habilidades():
+    return render_template('habilidades.html')
+
+@app.route('/documentos')
+def documentos():
+    return render_template('documentos.html')
+
+@app.route('/certificacoes')
+def certificacoes():
+    return render_template('certificacoes.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
